8892df529c272c2f995d9a6b972e1fe2c980a817
cx <2639340725@qq.com>
2024-07-26 11:02:50 +0800

    新增心跳检测机制,测验成功

--- network/TcpServe.cpp
+++ network/TcpServe.cpp
@@ -2,12 +2,22 @@
 
 TcpServe::TcpServe(QObject *parent) :QTcpServer(parent)
 {
+    this->time = new QTimer(this);
 
+    this->time->setInterval(50000);
+
+    this->time->start();
+
+    this->CheckHeartPack = false;
+
+    connect(time,&QTimer::timeout,this,&TcpServe::handlerHeart);//心跳检测 定时循环
+
+    qDebug()<<Q_FUNC_INFO<<"TcpServe类构造";
 }
 
 TcpServe::~TcpServe()
 {
-
+    qDebug()<<Q_FUNC_INFO<<"TcpServe类析构";
 }
 
 void TcpServe::incomingConnection(qintptr handle)
@@ -17,6 +27,7 @@ void TcpServe::incomingConnection(qintptr handle)
     connect(ClientSocket,&QTcpSocket::readyRead,this,&TcpServe::readClientData);
     connect(ClientSocket,&QTcpSocket::disconnected,this,&TcpServe::clientDisconnected);
     ClientSockets.append(ClientSocket);
+    HeartId.append(0);
 }
 
 void TcpServe::clientDisconnected()
@@ -24,6 +35,7 @@ void TcpServe::clientDisconnected()
     QTcpSocket * clientSocket = qobject_cast<QTcpSocket*>(sender());
     if(clientSocket)
     {
+        HeartId.removeAt(ClientSockets.indexOf(clientSocket));//移除检测位
         ClientSockets.removeOne(clientSocket);
         clientSocket->close();
         clientSocket->deleteLater();
@@ -37,15 +49,47 @@ void TcpServe::readClientData()
 
     QString Date = QString::fromUtf8(clientSocket->readAll());
 
-    if(Date.contains("SQL Requsted Find"))
+    if(Date.contains("SQL Requsted Find")) //sql查询返回菜单
         emit SQLfind(clientSocket,Date);
-    if(Date.contains("Food List")&&Date.contains("List End"))
+    else if(Date.contains("Food List")&&Date.contains("List End"))//客户包:菜单信息
     {
         Date.remove(0,9);//删头去尾
         Date.remove(Date.length() - 8, 8);
         Date.append("MenuList");
         emit ReceiveDate(clientSocket,Date);
     }
+    else if(Date.contains("Heart Palindrome"))//心跳检测回文包
+    {
+        HeartId[ClientSockets.indexOf(clientSocket)] = 1;
+    }
 
 
+}
+
+void TcpServe::handlerHeart()
+{
+    QString heartData = "10101111";//协议头
+    heartData.append("Heart Pack");
+    heartData.append("01011111");//协议尾
+
+    if(CheckHeartPack)
+    {
+        for(int i = 0;i < HeartId.size();i++)
+        {
+            if(HeartId.at(i) == 0)
+            {
+                this->ClientSockets[i]->close();
+                this->ClientSockets.removeAt(i);
+            }else
+            {
+                this->HeartId[i] = 0;
+            }
+        }
+    }
+
+    for(int i = 0;i < ClientSockets.size();i++)
+    {
+        ClientSockets[i]->write(heartData.toUtf8());
+    }
+    CheckHeartPack = true;
 }
--- network/TcpServe.h
+++ network/TcpServe.h
@@ -6,6 +6,8 @@
 #include <QThreadPool>
 #include <QObject>
 #include <QList>
+#include <QTimer>
+#include <QDebug>
 
 class TcpServe : public QTcpServer
 {
@@ -23,9 +25,12 @@ signals:
 private slots:
     void clientDisconnected(); //当产生客户端断开连接时向上层发送信号
     void readClientData();
-
+    void handlerHeart();//心跳处理槽函数
 private:
-    QList<QTcpSocket*> ClientSockets; //服务器Socket ID链表
+    QList<QTcpSocket*> ClientSockets; //服务器Socket链表
+    QList<int> HeartId;//心跳检测表
+    bool CheckHeartPack;
+    QTimer * time;
 };
 
 #endif
